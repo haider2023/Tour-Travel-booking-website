b5dd7521b4a0b7c0161ab1176a38abb345f78b68
Md Haider Ali <121243849+haider2023@users.noreply.github.com>
2022-12-31 21:06:48 +0530

    Update Script.js

--- Script.js
+++ Script.js
@@ -2,6 +2,7 @@
 let menu = document.querySelector('#menu-btn');
 let navbar= document.querySelector('.header .navbar');
 
+
 menu.onclick = () => {
     menu.classList.toggle('fa-times');
     navbar.classList.toggle('active');
